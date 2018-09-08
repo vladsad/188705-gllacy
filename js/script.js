552c165c90c3de4bc5c64cc38ab5237fe3d9670f
Vladislav Sadretdinov <vlad.sadretdinov@gmail.com>
2018-09-08 20:50:22 +0300

    Add filter price

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ var popup__overlay = document.querySelector(".modal__overlay");
 var close = popup.querySelector(".feedback__close");
 
 var form = popup.querySelector(".feedback__form");
-var login = popup.querySelector("[id=feedback-name]");
+var Name = popup.querySelector("[id=feedback-name]");
 var msg = popup.querySelector(".feedback__input--textarea");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("feedback--show");
   popup__overlay.classList.add("modal__overlay--show");
-  login.focus();
+  Name.focus();
 });
 
 close.addEventListener("click", function (evt) {
@@ -19,24 +19,25 @@ close.addEventListener("click", function (evt) {
   popup.classList.remove("feedback--show");
   popup.classList.remove("feedback--error");
   popup__overlay.classList.remove("modal__overlay--show");
+  msg.classList.remove("feedback__input--error");
 });
 
 form.addEventListener("submit", function (evt) {
   if (!msg.value) {
     evt.preventDefault();
-    popup.classList.add("feedback--error");
+    popup.classList.remove("feedback--error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("feedback--error");
+    msg.classList.add("feedback__input--error");
   }
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("feedback--show")) {
-      popup.classList.remove("feedback--show");
-      popup.classList.remove("feedback--error");
-      popup__overlay.classList.remove("modal__overlay--show");
-    }
+    popup.classList.toggle("feedback--show",false);
+    popup.classList.remove("feedback--error");
+    popup__overlay.classList.remove("modal__overlay--show");
+    msg.classList.remove("feedback__input--error");
   }
 });
